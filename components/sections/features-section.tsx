f0305c1d8a470c319e51b909c2e68b7c012d1b77
amgoun studio <amgoundesign02@gmail.com>
2025-07-08 16:28:28 +0100

    title colred

--- components/sections/features-section.tsx
+++ components/sections/features-section.tsx
@@ -1,44 +1,68 @@
-"use client"
+"use client";
 
-import { Badge } from "@/components/ui/badge"
-import AnimatedSection from "@/components/animated-section"
-import { staggerContainer, staggerItem } from "@/lib/variants"
-import { motion, Variants } from "framer-motion"
-import { features } from "@/lib/features-data"
-import Image from "next/image"
+import { Badge } from "@/components/ui/badge";
+import AnimatedSection from "@/components/animated-section";
+import { staggerContainer, staggerItem } from "@/lib/variants";
+import { motion, Variants } from "framer-motion";
+import { features } from "@/lib/features-data";
+import Image from "next/image";
 
 export default function FeaturesSection() {
   return (
-    <AnimatedSection id="features" className="bg-white py-12  md:py-16 lg:py-24">
+    <AnimatedSection
+      id="features"
+      className="bg-white py-12  md:py-16 lg:py-24"
+    >
       <div className="container-padding">
-      <div className="text-center space-y-4 mb-20">
-  <Badge className="bg-[#1BCEE0]/10 text-[#1E63A3] font-medium border-0">Özellikler</Badge>
-  <h2 className="text-4xl font-bold font-sans text-[#323232]">
-    Cildinizin Neye İhtiyacı Olduğunu <br /> <span className="text-gradient-primary">Yapay Zeka Destekli Analizle</span> Öğrenin
-  </h2>
-  <p className="text-xl text-[#323232]/70 max-w-2xl mx-auto font-sans">
-    Gelişmiş yapay zeka teknolojimiz cilt durumunuzu analiz eder ve en iyi cilt bakım sonuçları için size özel öneriler sunar.
-  </p>
-</div>
+        <div className="text-center space-y-4 mb-20">
+          <Badge className="bg-[#1BCEE0]/10 text-[#1E63A3] font-medium border-0">
+            Özellikler
+          </Badge>
+          <h2 className="text-4xl font-bold font-sans text-[#323232]">
+            Cildinizin Neye İhtiyacı Olduğunu <br />{" "}
+            <span className="text-gradient-primary">
+              Yapay Zeka Destekli 
+            </span>{" "}
+            Analizle Öğrenin
+          </h2>
+          <p className="text-xl text-[#323232]/70 max-w-2xl mx-auto font-sans">
+            Gelişmiş yapay zeka teknolojimiz cilt durumunuzu analiz eder ve en
+            iyi cilt bakım sonuçları için size özel öneriler sunar.
+          </p>
+        </div>
 
-
-        <motion.div className="space-y-12 md:space-y-28 " variants={staggerContainer(0.2, 0.1)} initial="hidden" animate="show">
+        <motion.div
+          className="space-y-12 md:space-y-28 "
+          variants={staggerContainer(0.2, 0.1)}
+          initial="hidden"
+          animate="show"
+        >
           {features.map((feature, index) => (
             <motion.div
               key={feature.id}
               variants={staggerItem() as Variants}
-              className={`flex flex-col ${index % 2 === 0 ? "lg:flex-row" : "lg:flex-row-reverse"} gap-8 items-center `}
+              className={`flex flex-col ${
+                index % 2 === 0 ? "lg:flex-row" : "lg:flex-row-reverse"
+              } gap-8 items-center `}
             >
               {/* Text Content */}
               <div className="flex-1 space-y-8">
                 <div className="space-y-6">
                   <h3 className="text-3xl font-bold text-[#323232] font-sans flex items-center gap-3">
-                    <span className="text-2xl md:text-3xl w-10 h-10 p-4 md:p-8 rounded-full flex items-center justify-center" style={{ backgroundColor: feature.iconBgColor , color: feature.iconColor }}>{feature.number}
-
+                    <span
+                      className="text-2xl md:text-3xl w-10 h-10 p-4 md:p-8 rounded-full flex items-center justify-center"
+                      style={{
+                        backgroundColor: feature.iconBgColor,
+                        color: feature.iconColor,
+                      }}
+                    >
+                      {feature.number}
                     </span>
                     {feature.title}
                   </h3>
-                  <p className="text-lg text-[#323232]/70 font-sans leading-relaxed">{feature.description}</p>
+                  <p className="text-lg text-[#323232]/70 font-sans leading-relaxed">
+                    {feature.description}
+                  </p>
                 </div>
 
                 {/* Feature Points */}
@@ -49,9 +73,14 @@ export default function FeaturesSection() {
                         className="w-8 h-8 md:w-10 md:h-10 rounded-full flex items-center justify-center flex-shrink-0"
                         style={{ backgroundColor: feature.iconBgColor }}
                       >
-                        <point.icon className="h-4 w-4 md:w-5 md:h-5" style={{ color: feature.iconColor }} />
+                        <point.icon
+                          className="h-4 w-4 md:w-5 md:h-5"
+                          style={{ color: feature.iconColor }}
+                        />
                       </div>
-                      <p className="text-[#323232]/80 font-sans leading-relaxed pt-2 font-semibold">{point.text}</p>
+                      <p className="text-[#323232]/80 font-sans leading-relaxed pt-2 font-semibold">
+                        {point.text}
+                      </p>
                     </div>
                   ))}
                 </div>
@@ -59,22 +88,20 @@ export default function FeaturesSection() {
 
               {/* Image */}
               <div className="flex-1 relative">
-               
-                  <div className="relative max-w-lg">
-                    <Image
-                      src={feature.image || "/placeholder.svg"}
-                      alt={feature.title}
-                      width={400}
-                      height={400}
-                      className="w-full h-auto rounded-3xl shadow-md"
-                    />
-                  </div>
-               
+                <div className="relative max-w-lg">
+                  <Image
+                    src={feature.image || "/placeholder.svg"}
+                    alt={feature.title}
+                    width={400}
+                    height={400}
+                    className="w-full h-auto rounded-3xl shadow-md"
+                  />
+                </div>
               </div>
             </motion.div>
           ))}
         </motion.div>
       </div>
     </AnimatedSection>
-  )
+  );
 }
--- components/sections/hero-section.tsx
+++ components/sections/hero-section.tsx
@@ -1,31 +1,41 @@
-"use client"
+"use client";
 
-import { motion, type Variants, AnimatePresence, useInView } from "framer-motion"
-import Link from "next/link"
-import { Sparkles } from "lucide-react"
-import PhoneSlider from "@/components/phone-slider"
-import AnimatedCard from "@/components/animated-card"
-import AnimatedButton from "@/components/animated-button"
-import { fadeIn,  } from "@/lib/variants"
-import Image from "next/image"
-import { useRef } from "react"
+import {
+  motion,
+  type Variants,
+  AnimatePresence,
+  useInView,
+} from "framer-motion";
+import Link from "next/link";
+import { Sparkles } from "lucide-react";
+import PhoneSlider from "@/components/phone-slider";
+import AnimatedCard from "@/components/animated-card";
+import AnimatedButton from "@/components/animated-button";
+import { fadeIn } from "@/lib/variants";
+import Image from "next/image";
+import { useRef } from "react";
 
 export default function HeroSection() {
-  const fadeUpAnimation = fadeIn("up", 0.3) as Variants
-  const fadeUpDelayedAnimation = fadeIn("up", 0.4) as Variants
+  const fadeUpAnimation = fadeIn("up", 0.3) as Variants;
+  const fadeUpDelayedAnimation = fadeIn("up", 0.4) as Variants;
 
-
-  const buttonsRef = useRef(null)
-  const isButtonsInView = useInView(buttonsRef, { once: true, margin: "-100px" })
+  const buttonsRef = useRef(null);
+  const isButtonsInView = useInView(buttonsRef, {
+    once: true,
+    margin: "-100px",
+  });
 
   return (
     <AnimatePresence mode="wait">
-      <section id="hero" className="container-padding  relative overflow-hidden mt-10 md:mt-20">
+      <section
+        id="hero"
+        className="container-padding  relative overflow-hidden mt-10 md:mt-20"
+      >
         {/* Try Products Button */}
-        <motion.div 
-          className="text-center mb-8" 
+        <motion.div
+          className="text-center mb-8"
           variants={fadeUpAnimation}
-          initial="hidden" 
+          initial="hidden"
           whileInView="show"
           viewport={{ once: true }}
         >
@@ -44,33 +54,37 @@ export default function HeroSection() {
           viewport={{ once: true }}
         >
           <h1 className="text-4xl lg:text-5xl font-bold leading-tight mb-6 font-sans ">
-          <span className="text-gradient-primary">Yapay Zeka Destekli</span> <br />
-             <span className="text-[#323232]">Analizle
-             Cilt Bakımını Zirveye Taşı!</span>
+            <span className="text-gradient-primary">Yapay Zeka Destekli</span> Analizle
+             <br />
+            <span className="text-[#323232]">Cilt Bakımını Zirveye Taşı!</span>
           </h1>
           <p className="text-lg text-[#323232]/70 max-w-2xl mx-auto leading-relaxed font-sans">
-  Gelişmiş yapay zeka teknolojisiyle sana en uygun cilt bakım rutinini keşfet. <br />
-  Kişiselleştirilmiş öneriler al ve cildindeki değişimi adım adım takip et!
-</p>
+            Gelişmiş yapay zeka teknolojisiyle sana en uygun cilt bakım rutinini
+            keşfet. <br />
+            Kişiselleştirilmiş öneriler al ve cildindeki değişimi adım adım
+            takip et!
+          </p>
         </motion.div>
 
         {/* Phone Mockup with Floating Elements */}
         <div className="relative flex justify-center items-center min-h-[600px]">
           {/* Background Gradient Blobs */}
-          <motion.div
-
-            className="absolute top-24  transform  -translate-x-40 w-[350px] h-[450px] bg-gradient-to-br from-[#C838F8]/60 via-[#6B88EB]/30 to-[#1BCEE0]/60 rounded-full opacity-90 blur-3xl z-0"
-          />
-             <motion.div
-
-className="absolute top-24  transform  translate-x-40 w-[350px] h-[450px] bg-gradient-to-br from-[#C838F8]/60 via-[#6B88EB]/30 to-[#1BCEE0]/60 rounded-full opacity-70 blur-3xl z-0"
-/>
-          
+          <motion.div className="absolute top-24  transform  -translate-x-40 w-[350px] h-[450px] bg-gradient-to-br from-[#C838F8]/60 via-[#6B88EB]/30 to-[#1BCEE0]/60 rounded-full opacity-90 blur-3xl z-0" />
+          <motion.div className="absolute top-24  transform  translate-x-40 w-[350px] h-[450px] bg-gradient-to-br from-[#C838F8]/60 via-[#6B88EB]/30 to-[#1BCEE0]/60 rounded-full opacity-70 blur-3xl z-0" />
 
           {/* Left Floating Card - Cleanser */}
-          <AnimatedCard delay={0.6} direction="left" className="absolute left-1/2 top-75 transform -translate-x-1/2 z-10 hidden md:block">
+          <AnimatedCard
+            delay={0.6}
+            direction="left"
+            className="absolute left-1/2 top-75 transform -translate-x-1/2 z-10 hidden md:block"
+          >
             <div className="bg-white rounded-2xl p-2 shadow-brand-lg max-w-xs -mr-20 ">
-            <Image src="/images/card-left.png" alt="Serum" width={203} height={350} />
+              <Image
+                src="/images/card-left.png"
+                alt="Serum"
+                width={203}
+                height={350}
+              />
             </div>
           </AnimatedCard>
 
@@ -81,7 +95,12 @@ className="absolute top-24  transform  translate-x-40 w-[350px] h-[450px] bg-gra
             className="absolute right-1/2 top-75 transform translate-x-1/2 z-10 hidden md:block"
           >
             <div className="bg-white rounded-2xl p-2 shadow-brand-lg max-w-xs -ml-20">
-              <Image src="/images/card-right-2.png" alt="Serum" width={203} height={350} />
+              <Image
+                src="/images/card-right-2.png"
+                alt="Serum"
+                width={203}
+                height={350}
+              />
             </div>
           </AnimatedCard>
 
@@ -93,37 +112,41 @@ className="absolute top-24  transform  translate-x-40 w-[350px] h-[450px] bg-gra
 
         {/* Feature Tags */}
         <div ref={buttonsRef}>
-
-        <motion.div
-          className="flex flex-wrap justify-center gap-4 mt-10 md:mt-16 px-4"
-          variants={fadeIn("up", 0)  as Variants}
-          initial="hidden"
-          animate={isButtonsInView ? "show" : "hidden"}
-        
-        >
-         <div className="flex flex-wrap justify-center gap-4  px-4">
-          <Link href="https://apps.apple.com/us/app/new-skin-scan-stop-wrinkles/id6739453891" target="_blank">
-          <Image
+          <motion.div
+            className="flex flex-wrap justify-center gap-4 mt-10 md:mt-16 px-4"
+            variants={fadeIn("up", 0) as Variants}
+            initial="hidden"
+            animate={isButtonsInView ? "show" : "hidden"}
+          >
+            <div className="flex flex-wrap justify-center gap-4  px-4">
+              <Link
+                href="https://apps.apple.com/us/app/new-skin-scan-stop-wrinkles/id6739453891"
+                target="_blank"
+              >
+                <Image
                   src="/images/store-btn.svg"
                   alt="Download on the App Store"
                   className="h-12 w-auto transition-transform duration-200 ease-in-out hover:scale-105 hover:-translate-y-1 active:scale-95"
                   width={180}
                   height={60}
                 />
-          </Link>
-          <Link href="https://apps.apple.com/us/app/new-skin-scan-stop-wrinkles/id6739453891" target="_blank">
-          <Image
+              </Link>
+              <Link
+                href="https://apps.apple.com/us/app/new-skin-scan-stop-wrinkles/id6739453891"
+                target="_blank"
+              >
+                <Image
                   src="/images/google-btn.svg"
                   alt="Download on the App Store"
                   className="h-12 w-auto transition-transform duration-200 ease-in-out hover:scale-105 hover:-translate-y-1 active:scale-95"
                   width={180}
                   height={60}
                 />
-          </Link>
-         </div>
-        </motion.div>
+              </Link>
+            </div>
+          </motion.div>
         </div>
       </section>
     </AnimatePresence>
-  )
+  );
 }
